4e69766d8cffae43861970972f8f7002e266e0d2
dSandley20 <danielsandley2@gmail.com>
2021-08-24 12:49:13 -0400

    added viewBinding to the main/login/register activities

--- app/src/main/java/com/example/snowtrails/activities/LoginActivity.kt
+++ app/src/main/java/com/example/snowtrails/activities/LoginActivity.kt
@@ -1,4 +1,14 @@
 package com.example.snowtrails.activities
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.snowtrails.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity (){
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
--- app/src/main/java/com/example/snowtrails/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/snowtrails/activities/RegisterActivity.kt
@@ -1,4 +1,15 @@
 package com.example.snowtrails.activities
 
-class RegisterActivity {
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.snowtrails.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity (){
+    private lateinit var binding : ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
