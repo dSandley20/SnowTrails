c259e2d1421f27ed1f851033ad0e24f9d2675122
dSandley20 <danielsandley2@gmail.com>
2021-08-25 00:58:47 -0400

    add login logic

--- app/src/main/java/com/example/snowtrails/activities/LoginActivity.kt
+++ app/src/main/java/com/example/snowtrails/activities/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.snowtrails.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.snowtrails.api.Api
 import com.example.snowtrails.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity (){
@@ -10,9 +12,30 @@ class LoginActivity : AppCompatActivity (){
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //TODO add validation for password and username
+
+        //logic for button on clicks
+        binding.logInSubmit.setOnClickListener{
+            submitLogin()
+        }
+
+        binding.goToRegisterButton.setOnClickListener {
+            goToRegister()
+        }
     }
 
-    fun submitLogin(){
 
+    fun submitLogin(){
+        var api = Api(this)
+        var loginData = HashMap<String, String>()
+        loginData.put("username", binding.usernameLoginInput.text.toString())
+        loginData.put("password", binding.passwordLoginInput.text.toString())
+        api.makeRequest(api.getRequestType("POST"), "/createuser", loginData)
+    }
+
+    fun goToRegister(){
+        var intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
