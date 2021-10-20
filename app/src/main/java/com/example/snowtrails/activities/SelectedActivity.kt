96f3b72d179982881814e0aa0a0f19078253980a
dSandley20 <danielsandley2@gmail.com>
2021-10-20 16:26:46 -0400

    handles auth user and directing to chat fragment or login fragment

--- app/src/main/java/com/example/snowtrails/activities/SelectedActivity.kt
+++ app/src/main/java/com/example/snowtrails/activities/SelectedActivity.kt
@@ -3,6 +3,7 @@ package com.example.snowtrails.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -11,6 +12,13 @@ import com.example.snowtrails.databinding.ActivitySelectedBinding
 import com.example.snowtrails.fragments.ChatLocationFragment
 import com.example.snowtrails.fragments.MainLocationFragment
 import com.example.snowtrails.fragments.MapLocationFragment
+import com.example.snowtrails.fragments.NoAuthFragment
+import com.example.snowtrails.services.AuthService
+import com.example.snowtrails.services.PoolService
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class SelectedActivity : AppCompatActivity() {
@@ -20,11 +28,19 @@ class SelectedActivity : AppCompatActivity() {
         binding = ActivitySelectedBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //getting data from the intent
         val locationData: com.example.snowtrails.room.entities.Location? =
             intent.getParcelableExtra("Location")
+        val isLoggedIn = intent.getBooleanExtra("isLoggedIn", false)
+        val authUser: com.example.snowtrails.room.entities.AuthenticatedUser? =
+            intent.getParcelableExtra("authUser")
 
         //setting bundle outside of check so other function can use data
-        val bundle = bundleOf("location_data" to locationData)
+        val bundle = bundleOf(
+            "location_data" to locationData,
+            "isLoggedIn" to isLoggedIn,
+            "authUser" to authUser
+        )
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
@@ -42,31 +58,33 @@ class SelectedActivity : AppCompatActivity() {
         }
 
         binding.locationChatButton.setOnClickListener {
-            goToChatFragment(bundle)
+            goToChatFragment(bundle, isLoggedIn)
         }
     }
 
-    private fun goToMainFragment(bundle: Bundle){
+    private fun goToMainFragment(bundle: Bundle) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             replace<MainLocationFragment>(R.id.location_fragment_container, args = bundle)
         }
     }
 
-    private fun goToMapFragment(bundle: Bundle){
+    private fun goToMapFragment(bundle: Bundle) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             replace<MapLocationFragment>(R.id.location_fragment_container, args = bundle)
         }
     }
 
-    private fun goToChatFragment(bundle: Bundle){
+    private fun goToChatFragment(bundle: Bundle, isLoggedIn: Boolean) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            replace<ChatLocationFragment>(R.id.location_fragment_container, args = bundle)
+            if (isLoggedIn) {
+                replace<ChatLocationFragment>(R.id.location_fragment_container, args = bundle)
+            } else {
+                replace<NoAuthFragment>(R.id.location_fragment_container, args = bundle)
+            }
         }
     }
 
-
 }
-
